02b669f1cd17cbc8d106ef3e87bcad4ac7681aad
Albert Agelviz <aag@hellofresh.com>
2017-08-24 12:24:31 +0200

    Using api to fetch data.

--- web/src/components/AuctionList.vue
+++ web/src/components/AuctionList.vue
@@ -1,51 +1,50 @@
 <template>
-    <div class ="container">
-        <div class ="row">
-            <div v-for="item in items" class="col-md-4">
-                <auction-list-item
-                    :name="item.name"
-                    :title="item.title"
-                    :description="item.description"
-                    :image="item.image"
-                ></auction-list-item>
-            </div>
-        </div>
-    </div>
+    <ul>
+        <li v-for="auction in auctions">
+            <auction-list-item
+                :ingredient="auction.ingredient"
+                :qty="auction.qty"
+                :type="filter"
+            >
+            </auction-list-item>
+        </li>
+    </ul>
 </template>
 
 <script>
     import AuctionListItem from '@/components/AuctionListItem'
+    import axios from 'axios';
+    import config from '@/config'
 
     export default {
         components: {
             AuctionListItem
         },
 
-        data() {
-            return {
-                items: [
-                    {
-                        'name' : 'Running',
-                        'title' : 'The current running',
-                        'description' : 'Lorem ipsum blah blah',
-                        'image' : 'https://cdn0.iconfinder.com/data/icons/shopping-and-retail/80/Shopping_retail-06-512.png'
-                    },
-                    {
-
-                        'name'  : 'Scheduled',
-                        'title' : 'The current scheduled',
-                        'description': 'Lorem ipsum blah blah',
-                        'image' : 'https://cdn4.iconfinder.com/data/icons/ios7-line/512/Timetable.png'
-                    },
-                    {
-                        'name' : 'Completed',
-                        'title': 'The bids completed',
-                        'description': 'Lorem ipsum blah blah blah',
-                        'image' : 'https://cdn0.iconfinder.com/data/icons/flat-security-icons/512/tick.png'
-                    }
-                ]
+        props: {
+            filter: {
+                type: String
             }
         },
+
+        data() {
+            return {
+                auctions: []
+            }
+        },
+
+        mounted() {
+            this.getResponseFrom(`auctions?s=${this.filter}`)
+        },
+
+        methods: {
+            getResponseFrom(url) {
+                axios.get(`${config.api.uri}/${url}`)
+                     .then(response => {
+                         this.auctions = response.data
+                     })
+            }
+        }
     }
 </script>
 
