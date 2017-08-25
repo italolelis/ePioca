75812765a3d712fb0511224554e5a559f18fc2c7
Kieran <me@kieranajp.co.uk>
2017-08-25 22:47:38 +0200

    aahahh

--- web/src/components/AppHeader.vue
+++ web/src/components/AppHeader.vue
@@ -1,9 +1,52 @@
 <template>
     <b-jumbotron>
         <router-link :to="{ name: 'Dashboard' }">
-            <img src="/static/epioca.png" width="200">
+            <img src="/static/epioca.png" class="epioca-logo">
 
-            <img class="pull-right" src="/static/chicken.png" width="100">
+            <img class="hf-logo pull-right" src="/static/chicken.png">
         </router-link>
     </b-jumbotron>
 </template>
+
+<script>
+import { getUserRole } from '@/api'
+
+export default {
+    computed: {
+        image() {
+            if (getUserRole() === 'supplier') {
+                return 'robert-approved.png'
+            }
+
+            return 'chicken.png'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.hf-logo {
+    width: 50px;
+}
+
+.epioca-logo {
+    width: 150px;
+}
+
+@media screen and (min-width: 320px) {
+    .hf-logo {
+        width: 75px;
+    }
+}
+
+@media screen and (min-width: 400px) {
+    .hf-logo {
+        width: 100px;
+    }
+
+    .epioca-logo {
+        width: 200px;
+    }
+}
+
+</style>
